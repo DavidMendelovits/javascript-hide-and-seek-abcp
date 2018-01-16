faf2b0e9129d6298fcb810b2a3e0bd4961833e6d
David Mendelovits <david.mendelovits@gmail.com>
2018-01-16 13:27:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ for(let i = 0; i < lis.length-1; i++) {
   }
   return test;
 }
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j =0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}

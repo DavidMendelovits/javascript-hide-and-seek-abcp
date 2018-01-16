ed53352058d7805d44c1b5ddb2dae720d2940009
David Mendelovits <david.mendelovits@gmail.com>
2018-01-16 13:15:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget() {
 function deepestChild() {
   var grandNode = document.querySelector('#grand-node');
   var deepestNode = grandNode.children[0];
-  for (var i = 0; !deepestNode.children[i]; i) {
+  for (var i = 0; !deepestNode.children[i]; i++) {
     deepestNode = deepestNode.children[0]
   }
   return deepestNode;
